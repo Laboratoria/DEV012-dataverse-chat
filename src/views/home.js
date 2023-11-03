d7565b42dbe0cce7fa65d11c5f3e4a22b6ed2e53
Juliana Guzmán <juliguzmac1906@gmail.com>
2023-11-02 19:19:00 -0500

    Se crea el componente renderCards en el archivo renderCards.js, se crea la primera vista sin funcionalidad en el archivo home.js y se ajusta el index.js

--- src/views/home.js
+++ src/views/home.js
@@ -1,16 +1,23 @@
 import {footer} from "../components/footer.js"
 import { header } from "../components/header.js";
+import { filterOrderBar } from "../components/filterOrderBar.js";
+import { renderCards } from "../components/renderCards.js";
+import data from "../data/data.js";
 
-//Aquí se pone el componente del html, se ejecuta la vista
 
 export const home = () => {
+  const homeView = document.createElement('div');
+  homeView.className = "viewComponent";
 
-  let homeView = header
+  const filterOrderBarComponent = filterOrderBar();
+  const headerComponent = header();
+  const footerComponent = footer();
+  const renderCardsComponent = renderCards(data);
+  
+  homeView.appendChild(headerComponent);
+  homeView.appendChild(filterOrderBarComponent);
+  homeView.appendChild(renderCardsComponent);
+  homeView.appendChild(footerComponent);
 
-  homeView += footer;
-
-  console.log(homeView);
-  return homeView
-
-
+  return homeView;
 }
