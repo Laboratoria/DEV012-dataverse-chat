3d3c649e79256343b60b787a8e84765aca820ebe
Diveana Inciarte <dinciarte25@gmail.com>
2023-11-04 13:17:36 -0300

    Se trasladan todos los eventos al home. Queda pendiente la creación de la  vista de facts para pasar el evento de la función de estadísticas.

--- src/views/home.js
+++ src/views/home.js
@@ -2,7 +2,7 @@ import { footer } from "../components/footer.js";
 import { header } from "../components/header.js";
 import { filterOrderBar } from "../components/filterOrderBar.js";
 import { renderCards } from "../components/renderCards.js";
-import { filterData } from "../lib/dataFunctions.js";
+import { filterData, sortData, sortBounty } from "../lib/dataFunctions.js";
 import data from "../data/data.js";
 
 export const home = () => {
@@ -10,33 +10,115 @@ export const home = () => {
   const homeView = document.createElement("div");
   homeView.className = "viewComponent";
 
-  const filterOrderBarComponent = filterOrderBar();
+  //renderizado original del componente home
+
   const headerComponent = header();
+  const filterOrderBarComponent = filterOrderBar();
   const footerComponent = footer();
   const renderCardsComponent = renderCards(result);
 
-  homeView.appendChild(headerComponent);
-  homeView.appendChild(filterOrderBarComponent);
+  //función que agrupa el renderizado del header y el filterbarcomponent
+  const headerFilterFunc = () => {
+    homeView.appendChild(headerComponent);
+    homeView.appendChild(filterOrderBarComponent);
+  };
+
+  //renderizado de home original
+  headerFilterFunc();
   homeView.appendChild(renderCardsComponent);
   homeView.appendChild(footerComponent);
 
-  //eventos para funcionalidad de los filtros.
+  //llamado de selectores para interacción con eventos
+  const filterOrigin = homeView.querySelector(
+    '[data-testid="select-filterOrigin"]'
+  );
+  const filterCrew = homeView.querySelector('[data-testid="select-filter"]');
+  const filterStatus = homeView.querySelector(
+    '[data-testid="select-filterStatus"]'
+  );
+  const sortName = homeView.querySelector('[data-testid="select-sort"]');
+  const sortedBounty = homeView.querySelector('[data-testid="select-bounty"]');
+  const clearButton = homeView.querySelector('[data-testid="button-clear"]');
 
-  const filterOrigin = homeView.querySelector('[data-testid="select-filterOrigin"]');
+  //eventos para funcionalidad de los filtros.
 
   filterOrigin.addEventListener("change", (e) => {
     e.preventDefault();
     const value = filterOrigin.value;
     const filteredOrigin = filterData(data, "seaOfOrigin", value);
     homeView.innerHTML = "";
-    // filterCrew.value = "";
-    // filterStatus.value = "";
+    filterCrew.value = "";
+    filterStatus.value = "";
     const filteredList = renderCards(filteredOrigin);
-    homeView.appendChild(headerComponent);
-    homeView.appendChild(filterOrderBarComponent);
+    headerFilterFunc();
     homeView.appendChild(filteredList);
     homeView.appendChild(footerComponent);
     result = filteredOrigin;
+  });
+
+  filterCrew.addEventListener("change", (e) => {
+    e.preventDefault();
+    const value = filterCrew.value;
+    const filteredCrew = filterData(data, "crewOrigin", value);
+    homeView.innerHTML = "";
+    filterOrigin.value = "";
+    filterStatus.value = "";
+    const filteredList = renderCards(filteredCrew);
+    headerFilterFunc();
+    homeView.appendChild(filteredList);
+    homeView.appendChild(footerComponent);
+    result = filteredCrew;
+  });
+
+  filterStatus.addEventListener("change", (e) => {
+    e.preventDefault();
+    const value = filterStatus.value;
+    const filteredStatus = filterData(data, "status", value);
+    homeView.innerHTML = "";
+    filterOrigin.value = "";
+    filterCrew.value = "";
+    const filteredList = renderCards(filteredStatus);
+    headerFilterFunc();
+    homeView.appendChild(filteredList);
+    homeView.appendChild(footerComponent);
+    result = filteredStatus;
+  });
+
+  sortName.addEventListener("change", (e) => {
+    e.preventDefault();
+    const sortOrder = sortName.value;
+    const sortedName = sortData(result, "name", sortOrder);
+    homeView.innerHTML = "";
+    // sortedBounty.value = "";
+    const sortedList = renderCards(sortedName);
+    headerFilterFunc();
+    homeView.appendChild(sortedList);
+    homeView.appendChild(footerComponent);
+  });
+
+  sortedBounty.addEventListener("change", (e) => {
+    e.preventDefault();
+    const sortOrder = sortedBounty.value;
+    const sortedResultBounty = sortBounty(result, sortOrder);
+    homeView.innerHTML = "";
+    sortName.value = "";
+    const sortedList = renderCards(sortedResultBounty);
+    headerFilterFunc();
+    homeView.appendChild(sortedList);
+    homeView.appendChild(footerComponent);
+  });
+
+  clearButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    homeView.innerHTML = "";
+    filterOrigin.value = "";
+    filterCrew.value = "";
+    filterStatus.value = "";
+    sortName.value = "";
+    sortedBounty.value = "";
+    headerFilterFunc();
+    homeView.appendChild(renderCards(data));
+    homeView.appendChild(footerComponent);
   });
 
   return homeView;
