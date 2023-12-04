45ff0e868672009871e672452c1d8c7805302c7d
Diveana Inciarte <dinciarte25@gmail.com>
2023-12-04 18:26:05 -0300

    Se arregla error en eventos de filtrado y sorteado y se elmina vista de chatConteinerAll

--- src/views/home.js
+++ src/views/home.js
@@ -8,10 +8,13 @@ import { navigateTo } from "../router.js";
 import { createBtnChat } from "../components/btnChat.js";
 
 export const home = () => {
-  let result = data.filter(character => character.id !== "all");
+  const dataWithoutIdAll = data.filter(character => character.id !== "all");
   const homeView = document.createElement("div");
   homeView.className = "viewComponent";
+  
+  let result = dataWithoutIdAll;
 
+  
   function setupRenderCards(cards) {
     const renderCardsComponent = renderCards(cards);
     // RenderCardsComponent es un elemento ul
@@ -31,7 +34,7 @@ export const home = () => {
   const headerComponent = header();
   const filterOrderBarComponent = filterOrderBar();
   //  const renderCardsComponent = renderCards(result);
-  const renderCardsComponent = setupRenderCards(result);
+  const renderCardsComponent = setupRenderCards(dataWithoutIdAll);
 
   const footerComponent = footer();
 
@@ -40,20 +43,28 @@ export const home = () => {
     homeView.appendChild(headerComponent);
     homeView.appendChild(filterOrderBarComponent);
   };
-  //creación del botón de chat with me y h2 con info de la pagina.
-  const divBtnChat = document.createElement("div");
-  divBtnChat.className = "h2-btn-chat"
-  const h2 = document.createElement("h2");
-  h2.textContent = "FIND MORE INFO OR TALK TO YOU FAVORITE PIRATE BY CLICKING THEM";
-  const buttonChatHome = createBtnChat("all", "CHAT WITH ALL");
-  //renderizado divBtnChat
-  divBtnChat.appendChild(h2);
-  divBtnChat.appendChild(buttonChatHome);
+
+  //función que crea el botón de chat with me y h2 con info de la pagina.
+  const subHeader = () => {
+    
+    const divBtnChat = document.createElement("div");
+    divBtnChat.className = "h2-btn-chat"
+    const h2 = document.createElement("h2");
+    h2.textContent = "FIND MORE INFO OR TALK TO YOU FAVORITE PIRATE BY CLICKING THEM";
+    const buttonChatHome = createBtnChat("all", "CHAT WITH ALL");
+    //renderizado divBtnChat
+    divBtnChat.appendChild(h2);
+    divBtnChat.appendChild(buttonChatHome);
+
+    return divBtnChat;
+
+  } 
+
 
 
   //renderizado de home original
   headerFilterFunc();
-  homeView.appendChild(divBtnChat)
+  homeView.appendChild(subHeader());
   homeView.appendChild(renderCardsComponent);
   homeView.appendChild(footerComponent);
 
@@ -75,46 +86,49 @@ export const home = () => {
   filterOrigin.addEventListener("change", (e) => {
     e.preventDefault();
     const value = filterOrigin.value;
-    const filteredOrigin = filterData(data, "seaOfOrigin", value);
+    const filteredOrigin = filterData(dataWithoutIdAll, "seaOfOrigin", value);
     homeView.innerHTML = "";
     filterCrew.value = "";
     filterStatus.value = "";
+    result = filteredOrigin;
     const filteredList = setupRenderCards(result);
     //renderizamos componentes del home junto a la función que llama el evento
     headerFilterFunc();
+    homeView.appendChild(subHeader());
     homeView.appendChild(filteredList);
     homeView.appendChild(footerComponent);
-    result = filteredOrigin;
   });
 
   filterCrew.addEventListener("change", (e) => {
     e.preventDefault();
     const value = filterCrew.value;
-    const filteredCrew = filterData(data, "crewOrigin", value);
+    const filteredCrew = filterData(dataWithoutIdAll, "crewOrigin", value);
     homeView.innerHTML = "";
     filterOrigin.value = "";
     filterStatus.value = "";
+    result = filteredCrew;
     const filteredList = setupRenderCards(result);
     //renderizamos componentes del home junto a la función que llama el evento
     headerFilterFunc();
+    homeView.appendChild(subHeader());
     homeView.appendChild(filteredList);
     homeView.appendChild(footerComponent);
-    result = filteredCrew;
   });
 
   filterStatus.addEventListener("change", (e) => {
     e.preventDefault();
     const value = filterStatus.value;
-    const filteredStatus = filterData(data, "status", value);
+    const filteredStatus = filterData(dataWithoutIdAll, "status", value);
     homeView.innerHTML = "";
     filterOrigin.value = "";
     filterCrew.value = "";
+    result = filteredStatus;
     const filteredList = setupRenderCards(result);
     //renderizamos componentes del home junto a la función que llama el evento
     headerFilterFunc();
+    homeView.appendChild(subHeader());
     homeView.appendChild(filteredList);
     homeView.appendChild(footerComponent);
-    result = filteredStatus;
   });
 
   sortName.addEventListener("change", (e) => {
@@ -123,9 +137,10 @@ export const home = () => {
     const sortedName = sortData(result, "name", sortOrder);
     homeView.innerHTML = "";
     sortedBounty.value = "";
-    const sortedList = setupRenderCards(result);
+    const sortedList = setupRenderCards(sortedName);
     //renderizamos componentes del home junto a la función que llama el evento
     headerFilterFunc();
+    homeView.appendChild(subHeader());
     homeView.appendChild(sortedList);
     homeView.appendChild(footerComponent);
   });
@@ -136,9 +151,10 @@ export const home = () => {
     const sortedResultBounty = sortBounty(result, sortOrder);
     homeView.innerHTML = "";
     sortName.value = "";
-    const sortedList = setupRenderCards(result);
+    const sortedList = setupRenderCards(sortedResultBounty);
     //renderizamos componentes del home junto a la función que llama el evento
     headerFilterFunc();
+    homeView.appendChild(subHeader());
     homeView.appendChild(sortedList);
     homeView.appendChild(footerComponent);
   });
@@ -151,9 +167,10 @@ export const home = () => {
     filterStatus.value = "";
     sortName.value = "";
     sortedBounty.value = "";
-    result = data;
+    result = dataWithoutIdAll;
     //renderizamos componentes del home junto a la función que llama el evento
     headerFilterFunc();
+    homeView.appendChild(subHeader());
     homeView.appendChild(setupRenderCards(result));
     homeView.appendChild(footerComponent);
   });
