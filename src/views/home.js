00948b03c1780b5000dc12bf95c07cf2ad9fc26e
camilanontoa <camilam.1024@gmail.com>
2023-12-11 16:33:24 -0500

    Ejemplo popState

--- src/views/home.js
+++ src/views/home.js
@@ -1,5 +1,11 @@
-const Example = () => {
+import buton from "../components/Button.js";
 
+const Example = () => {
+    const container = document.createElement('span');
+    const tittle = document.createElement('h1')
+    tittle.textContent='jajaja'
+    container.append(tittle, buton())
+    return container
 };
 
 export default Example;
