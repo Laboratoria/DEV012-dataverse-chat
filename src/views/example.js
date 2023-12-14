99856cd7a155df014c8fbf8a7cde3609697b8b76
vaneval <vaneval.dese@gmail.com>
2023-12-14 11:39:40 -0500

    add new changes

--- src/views/example.js
+++ src/views/example.js
@@ -1,6 +1,3 @@
 export const example = () => {
     return document.createElement('p').textContent("hola mundo");    
 };
-export const Example = () => {
-    return document.createElement('p').textContent("hola mundo");
-};
