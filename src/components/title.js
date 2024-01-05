fa9ac9555c952f1ec22d595f3b0591ad5b21c16f
camilanontoa <camilam.1024@gmail.com>
2024-01-05 11:42:31 -0500

    Api Key vista

---
+++ src/components/title.js
@@ -0,0 +1,7 @@
+export const Title = () => {
+    const Title = document.createElement("div");
+    Title.id = "div"
+    Title.innerHTML = `<div class=title> warsiepedia </div>`;
+
+    return Title
+}
