8c0ad564b850401e042217c36b96fb59b313cdb7
vaneval <vaneval.dese@gmail.com>
2024-01-12 12:42:50 -0500

    solución del error del botón de retorno

--- src/views/chat.js
+++ src/views/chat.js
@@ -1,11 +1,11 @@
 import { ChatPanel } from "../components/chatContainer.js";
-import { buton } from "../components/returnButton.js";
+import {ReturnButton } from "../components/returnButton.js";
 import { Footer } from "../components/footer.js";
 
 export const CharacterChat = () => {
     const chatView = document.createElement('body');
 
-    chatView.append(buton());
+    chatView.append(ReturnButton());
     const nameCharacter = document.createElement('p');
     nameCharacter.innerHTML = `Din Djarin (The Mandalorian)`
     chatView.appendChild(nameCharacter);
