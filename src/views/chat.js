fe5cc34162c2aa125a1c828b5e7f4e5708ac19ce
vaneval <vaneval.dese@gmail.com>
2024-01-23 00:33:56 -0500

    vista de chat movil

--- src/views/chat.js
+++ src/views/chat.js
@@ -1,24 +1,22 @@
 import { ChatPanel } from "../components/chatContainer.js";
-import {ReturnButton } from "../components/returnButton.js";
+import { ReturnButton } from "../components/returnButton.js";
 import { Footer } from "../components/footer.js";
 import { chatCompletions } from "../lib/openIaAPI.js";
 
 export const CharacterChat = () => {
-    const chatView = document.createElement('body');
+  const chatView = document.createElement("body");
 
-    //chatView.append();
-    //chatView.appendChild(nameCharacter);
+  //chatView.append();
+  //chatView.appendChild(nameCharacter);
 
-    chatView.append(ChatPanel(), ReturnButton(), Footer());
+  chatView.append(ChatPanel(), ReturnButton(), Footer());
 
-//chatView.querySelector('button#sendMessageBtn').addEventListener('click', () =>{
-    // console.log('okbtn');
+  //chatView.querySelector('button#sendMessageBtn').addEventListener('click', () =>{
+  // console.log('okbtn');
   //  const msg = chatView.querySelector('input#messageInput').value
-    //console.log(msg);
-    // chatCompletions(msg, personaje)
-//})
-    
+  //console.log(msg);
+  // chatCompletions(msg, personaje)
+  //})
 
-    return chatView
+  return chatView;
 };
-
